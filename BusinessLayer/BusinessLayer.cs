771e8d84c70ad2b5f4f36aec76efefb98a864b79
Tajbir Sandhu <sandhu_tajbir@outlook.com>
2019-10-20 13:39:27 -0700

    Add project files.

---
+++ BusinessLayer/BusinessLayer.cs
@@ -0,0 +1,123 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BusinessLayer
+{
+    public class BusinessLayer : IBusinessLayer
+    {
+        private readonly IStandardRepository _standardRepository;
+        private readonly ITeacherRepository _teacherRepository;
+        private readonly ICourseRepository _courseRepository;
+
+        public BusinessLayer()
+        {
+            _standardRepository = new StandardRepository();
+            _teacherRepository = new TeacherRepository();
+            _courseRepository = new CourseRepository();
+        }
+
+        #region Standard
+        public IEnumerable<Standard> GetAllStandards()
+        {
+            return _standardRepository.GetAll();
+        }
+
+        public Standard GetStandardByID(int id)
+        {
+            return _standardRepository.GetById(id);
+        }
+
+        public Standard GetStandardByName(string name)
+        {
+            return _standardRepository.GetSingle(
+                s => s.StandardName.Equals(name),
+                s => s.Students);
+        }
+
+        public void AddStandard(Standard standard)
+        {
+            _standardRepository.Insert(standard);
+        }
+
+        public void UpdateStandard(Standard standard)
+        {
+            _standardRepository.Update(standard);
+        }
+
+        public void RemoveStandard(Standard standard)
+        {
+            _standardRepository.Delete(standard);
+        }
+        #endregion
+
+        #region Teacher
+        public IEnumerable<Teacher> GetAllTeachers()
+        {
+            return _teacherRepository.GetAll();
+        }
+
+        public Teacher GetTeacherByID(int id)
+        {
+            return _teacherRepository.GetById(id);
+        }
+
+        public Teacher GetTeacherByName(string name)
+        {
+            return _teacherRepository.GetSingle(
+                s => s.TeacherName.Equals(name),
+                s => s.Standard);
+        }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            _teacherRepository.Insert(teacher);
+        }
+
+        public void UpdateTeacher(Teacher teacher)
+        {
+            _teacherRepository.Update(teacher);
+        }
+
+        public void RemoveTeacher(Teacher teacher)
+        {
+            _teacherRepository.Delete(teacher);
+        }
+        #endregion
+
+        #region Course
+        public IEnumerable<Course> GetAllCourses()
+        {
+            return _courseRepository.GetAll();
+        }
+
+        public Course GetCourseByID(int id)
+        {
+            return _courseRepository.GetById(id);
+        }
+
+        public Course GetCourseByName(string name)
+        {
+            return _courseRepository.GetSingle(
+                s => s.CourseName.Equals(name),
+                s => s.Teacher);
+        }
+
+        public void AddCourse(Course course)
+        {
+            _courseRepository.Insert(course);
+        }
+
+        public void UpdateCourse(Course course)
+        {
+            _courseRepository.Update(course);
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            _courseRepository.Delete(course);
+        }
+        #endregion
+    }
+}
---
+++ BusinessLayer/IBusinessLayer.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer;
+using System.Collections.Generic;
+
+namespace BusinessLayer
+{
+    public interface IBusinessLayer
+    {
+        #region Standard
+        IEnumerable<Standard> GetAllStandards();
+
+        Standard GetStandardByID(int id);
+
+        Standard GetStandardByName(string name);
+
+        void AddStandard(Standard standard);
+
+        void UpdateStandard(Standard standard);
+
+        void RemoveStandard(Standard standard);
+        #endregion
+
+        #region Teacher
+        IEnumerable<Teacher> GetAllTeachers();
+
+        Teacher GetTeacherByID(int id);
+
+        Teacher GetTeacherByName(string teacher);
+
+        void AddTeacher(Teacher teacher);
+
+        void UpdateTeacher(Teacher teacher);
+
+        void RemoveTeacher(Teacher teacher);
+        #endregion
+
+        #region Course
+        IEnumerable<Course> GetAllCourses();
+
+        Course GetCourseByID(int id);
+
+        Course GetCourseByName(string student);
+
+        void AddCourse(Course student);
+
+        void UpdateCourse(Course student);
+
+        void RemoveCourse(Course student);
+        #endregion
+
+    }
+}
